fd7ee4f19bda6d55145d2b144083a207d904c85b
LiChengWei0 <LCW541129@outlook.com>
2020-04-15 22:07:52 +0800

    Initial commit

---
+++ src/main/java/com/example/animation/PieActivity.java
@@ -0,0 +1,34 @@
+package com.example.animation;
+
+import com.example.animation.widget.PieAnimation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by ouyangshen on 2016/11/27.
+ */
+public class PieActivity extends AppCompatActivity implements OnClickListener {
+	private PieAnimation pa_circle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pie);
+		pa_circle = (PieAnimation) findViewById(R.id.pa_circle);
+		pa_circle.setOnClickListener(this);
+		pa_circle.start();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.pa_circle) {
+			if (pa_circle.isRunning() != true) {
+				pa_circle.start();
+			}
+		}
+	}
+
+}
